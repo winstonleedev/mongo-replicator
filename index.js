09d66843b305fe4b86094bb2c4dc36f894cb188e
Thanh Phu <thanh.phu@daliworks.net>
2018-05-24 18:19:55 +0900

    Simplify code + log errors

--- index.js
+++ index.js
@@ -14,45 +14,51 @@ const pipeline = [
   }
 ];
 
-MongoClient.connect("mongodb://localhost:27017")
+function logError(err) {
+  console.error(err);
+}
+
+MongoClient
+  .connect("mongodb://localhost:27017", { useNewUrlParser: true })
   .then(client => {
     console.log("Connected correctly to server");
     // specify db and collections
     const db = client.db("superheroesdb");
     const collection = db.collection("superheroes");
-    
-    const changeStream = collection.watch(pipeline);
+
+    const changeStream = collection.watch();
     // start listen to changes
     changeStream.on("change", function (change) {
       console.log(change);
     });
 
-    // insert few data with timeout so that we can watch it happening
-    setTimeout(function () {
-      collection.insert({ "batman": "bruce wayne" }, function (err) {
-        assert.ifError(err);
-      });
-    }, 1000);
-    setTimeout(function () {
-      collection.insert({ "superman": "clark kent" }, function (err) {
-        assert.ifError(err);
-      });
-    }, 2000);
-    setTimeout(function () {
-      collection.insert({ "wonder-woman": "diana prince" }, function (err) {
-        assert.ifError(err);
-      });
-    }, 3000);
-    setTimeout(function () {
-      collection.insert({ "ironman": "tony stark" }, function (err) {
-        assert.ifError(err);
-      });
-    }, 4000);
-    setTimeout(function () {
-      collection.insert({ "spiderman": "peter parker" }, function (err) {
-        assert.ifError(err);
-      });
-    }, 5000);
+    process.nextTick(function () {
+      // insert few data with timeout so that we can watch it happening
+      setTimeout(function () {
+        collection.insert({ "batman": "bruce wayne" }, logError);
+      }, 1000);
+      setTimeout(function () {
+        collection.insert({ "superman": "clark kent" }, logError);
+      }, 2000);
+      setTimeout(function () {
+        collection.insert({ "wonder-woman": "diana prince" }, logError);
+      }, 3000);
+      setTimeout(function () {
+        collection.insert({ "ironman": "tony stark" }, logError);
+      }, 4000);
+      setTimeout(function () {
+        collection.insert({ "spiderman": "peter parker" }, logError);
+      }, 5000);
+      // update existing document
+      setTimeout(function () {
+        collection.updateOne({ "ironman": "tony stark" }, { $set: { "ironman": "elon musk" } }, logError);
+      }, 6000);
+      // delete existing document
+      setTimeout(function () {
+        collection.deleteOne({ "spiderman": "peter parker" }, logError);
+      }, 7000);
+    });
+  })
+  .catch(err => {
+    console.error(err);
   });
-
-
