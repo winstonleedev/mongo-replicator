1aaa59abb7c7294f6487c43dc711292378fe2e6f
Thanh Phu <thanh.phu@daliworks.net>
2018-06-24 18:01:58 +0900

    String stored procedure

--- sql/insertNumericValue.sql
+++ sql/insertNumericValue.sql
@@ -1,4 +1,18 @@
-CREATE OR REPLACE FUNCTION public."insert_value_number"(mongo_id_sensor character varying, in_time timestamp without time zone, in_value NUMERIC)
+CREATE OR REPLACE FUNCTION public."insert_value_string"(mongo_id_sensor character varying, in_time timestamp, in_value character varying)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    _id_sensor integer;
+    _id_ts integer;
+BEGIN
+    _id_sensor = exist_sensor(mongo_id_sensor, 'string');
+    INSERT INTO series_string(id_sensor, "time", "value") VALUES (_id_sensor, in_time, in_value) RETURNING id INTO _id_ts;
+    RETURN _id_ts;
+END;
+$function$;
+
+CREATE OR REPLACE FUNCTION public."insert_value_number"(mongo_id_sensor character varying, in_time timestamp, in_value NUMERIC)
  RETURNS integer
  LANGUAGE plpgsql
 AS $function$
@@ -7,7 +21,7 @@ DECLARE
     _id_ts integer;
 BEGIN
     _id_sensor = exist_sensor(mongo_id_sensor, 'number');
-    INSERT INTO series_number(id_Sensor, "time", "value") VALUES (_id_sensor, in_time, in_value) RETURNING id INTO _id_ts;
+    INSERT INTO series_number(id_sensor, "time", "value") VALUES (_id_sensor, in_time, in_value) RETURNING id INTO _id_ts;
     RETURN _id_ts;
 END;
 $function$;
@@ -20,11 +34,12 @@ DECLARE
 BEGIN
     SELECT id_sensor INTO _id_sensor FROM sensors WHERE sensors.mongo_id_sensor = in_mongo_id_sensor AND sensors.type = in_type;
     IF NOT FOUND THEN
-        INSERT INTO sensors(mongo_id_sensor, "type") VALUES (sensor_id, in_type) RETURNING id_sensor into _id_sensor;
+        INSERT INTO sensors(mongo_id_sensor, "type") VALUES (in_mongo_id_sensor, in_type) RETURNING id_sensor into _id_sensor;
     END IF;
     RETURN _id_sensor;
 END;
 $function$  LANGUAGE plpgsql;
 
-SELECT exist_sensor('test', 'number');
-SELECT insert_value_number('test'::VARCHAR, LOCALTIMESTAMP, 10.0);
+--SELECT exist_sensor('test', 'number');
+--SELECT insert_value_number('test'::VARCHAR, LOCALTIMESTAMP, 10.0);
+SELECT insert_value_string('test3'::VARCHAR, LOCALTIMESTAMP, 'a string');
